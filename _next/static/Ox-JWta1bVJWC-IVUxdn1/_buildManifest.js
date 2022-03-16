cceb2228b57df97719a64d75fbff66c2bcc006a6
Tanay360 <tanaygupta270@gmail.com>
2022-03-16 20:20:26 +0530

    Initial commit

---
+++ _next/static/Ox-JWta1bVJWC-IVUxdn1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/900-ece2f75934e5a518.js","static/chunks/pages/index-e7edf135e3c00000.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
